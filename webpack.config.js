ce482fe600dbc748966313e9d9135fb9e4fece6d
kim SangHyuk <sanghyuk@kinterch.com>
2021-05-13 23:14:05 +0900

    webpack entry , out 실습

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry : {
+       main :'./src/app.js',
+
+    },
+    output: {
+        filename: '[name].js',
+        path:path.resolve('./dist'),
+    }
+}
