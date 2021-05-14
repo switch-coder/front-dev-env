107bad21e7cbfe8f8bb681f57d12ff8c641062d5
kim SangHyuk <sanghyuk@kinterch.com>
2021-05-14 17:31:04 +0900

    로더 실습 결과

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,24 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      },
+      {test: /\.(png|jpg)$/,
+        loader: 'file-loader',
+        options:{
+          publicPath: './dist/',
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
   }
 }
 
