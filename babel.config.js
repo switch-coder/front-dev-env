087572dad32aa9cdc0e734455a694887eac8055b
kim SangHyuk <sanghyuk@kinterch.com>
2021-05-15 15:16:57 +0900

    bable 실습 완룍

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 /**
  * TODO: IE11에서도 동작하는 자바스크립트로 변환할수 있게 바벨을 구성하세요.
  */
+
+ module.exports = {
+     presets:[
+         ['@babel/env',{
+             modules:false,
+             useBuiltIns : 'usage', 
+             targets: {
+                 ie:11,
+                 chrome:79
+                },
+            corejs: { 
+                version :2
+            }
+        }
+    ]
+     ]
+ }
