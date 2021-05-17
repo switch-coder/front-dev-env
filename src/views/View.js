5b18062cfe50bd48ecb3388bda530f52d1c4cd47
kim SangHyuk <sanghyuk@kinterch.com>
2021-05-17 23:31:02 +0900

    lint 실습 완료

--- src/views/View.js
+++ src/views/View.js
@@ -1,31 +1,31 @@
 export default class View {
-  constructor(el) {
-    if (!el) throw el;
+  constructor (el) {
+    if (!el) throw el
 
-    this.el = el;
-    return this;
+    this.el = el
+    return this
   }
 
-  on(event, handler) {
-    this.el.addEventListener(event, handler);
-    return this;
+  on (event, handler) {
+    this.el.addEventListener(event, handler)
+    return this
   }
 
-  emit(event, data) {
+  emit (event, data) {
     const evt = new CustomEvent(event, {
       detail: data
-    });
-    this.el.dispatchEvent(evt);
-    return this;
+    })
+    this.el.dispatchEvent(evt)
+    return this
   }
 
-  hide() {
-    this.el.style.display = "none";
-    return this;
+  hide () {
+    this.el.style.display = 'none'
+    return this
   }
 
-  show() {
-    this.el.style.display = "";
-    return this;
+  show () {
+    this.el.style.display = ''
+    return this
   }
 }
